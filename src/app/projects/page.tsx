5eae2263b31c8dc0acdf57e152639e67716018a5
Honzoraptor <j.ackermann@email.cz>
2024-05-02 16:19:04 +0200

    Redesigned posts page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,13 +7,50 @@ export const metadata: Metadata = {
   description: "Here you can see all of the projects I've coded so far.",
 };
 
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 async function getProjects() {
   const { data } = await supabase.from("Projects").select();
-  return data ?? [];
+  if (data) {
+    data.sort((a: any, b: any) => {
+      try {
+        return (
+          new Date(
+            `${b.date.split(".")[0]} ${
+              months[b.date.split(".")[1] - 1]
+            }, ${b.date.split(".")[2].slice(1)}`
+          ).valueOf() -
+          new Date(
+            `${a.date.split(".")[0]} ${
+              months[a.date.split(".")[1] - 1]
+            }, ${a.date.split(".")[2].slice(1)}`
+          ).valueOf()
+        );
+      } catch (error) {
+        console.error("An error occured while sorting the projects:", error);
+        return 0;
+      }
+    });
+
+    return data;
+  }
 }
 
 async function Projects() {
-  const projects = await getProjects();
+  const projects = (await getProjects()) ?? [];
   return (
     <main className="nav-space-top projects">
       <h1>My projects</h1>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 interface props {
   date: string;
@@ -36,21 +37,6 @@ function Project({
   const [copied, setCopied] = useState(false);
   const [currentLocationHash, setCurrentLocationHash] = useState("");
 
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
   useEffect(() => {
     setCurrentLocationHash(location.hash);
   }, []);
@@ -64,107 +50,145 @@ function Project({
       }`}
       id={title.replaceAll(" ", "-")}
     >
-      <div className="project-side">
-        <a target="_blank" href={githubUrl}>
+      <div className="project-side btn-gap project-side-wimg">
+        <a
+          target="_blank"
+          href={githubUrl}
+          className="project-img-link flex-center-all"
+        >
           <img src={imgUrl} className="rounded project-image" />
         </a>
+        <div className="tags-wrp desktop">
+          <p>Tags:</p>
+          <div className="tags">
+            {tags.map((v: any, i: any) => {
+              return (
+                <p key={i} className={`tag ${v}-tag`}>
+                  {v}
+                </p>
+              );
+            })}
+          </div>
+        </div>
       </div>
-      <div className="project-side">
-        <div className="project-top">
-          <h3>{title}</h3>
-          <p className="date">
-            {date.split(".")[0].length < 2
-              ? `0${date.split(".")[0]}`
-              : date.split(".")[0]}
-            .
-            {date.split(".")[1].length < 2
-              ? `0${date.split(".")[1]}`
-              : date.split(".")[1]}
-            . {date.split(".")[2].slice(1)}
-          </p>
-        </div>
-        <p>{text}</p>
-        <div className="project-links">
-          {!isWeb ? (
-            <a target="_blank" href={githubUrl} className="before-bg-anim">
-              <img src="/github.svg" className="no-select" />
-            </a>
-          ) : (
-            ""
-          )}
-
-          {isWeb && link !== "no xD" ? (
-            <>
-              <a target="_blank" href={githubUrl} className="before-bg-anim">
-                <img src="/github.svg" className="no-select" />
-              </a>
-              <a target="_blank" href={link} className="before-bg-anim">
-                <img src="/link-away.svg" className="no-select" />
-              </a>
-            </>
-          ) : (
-            <>
-              {isWeb && (
-                <>
+      <div className="project-side project-side-content">
+        <div className="project-text-info">
+          <div className="project-top">
+            <h3>{title}</h3>
+            <p className="date">
+              {date.split(".")[0].length < 2
+                ? `0${date.split(".")[0]}`
+                : date.split(".")[0]}
+              .
+              {date.split(".")[1].length < 2
+                ? `0${date.split(".")[1]}`
+                : date.split(".")[1]}
+              . {date.split(".")[2].slice(1)}
+            </p>
+          </div>
+          <p>{text}</p>
+          <div className="flex-between align-center">
+            <div className="tags-wrp mobile">
+              <p>Tags:</p>
+              <div className="tags">
+                {tags.map((v: any, i: any) => {
+                  return (
+                    <p key={i} className={`tag ${v}-tag`}>
+                      {v}
+                    </p>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="popover-wrp grid-wrp mobile">
+              <button
+                id={`btn-${index}`}
+                className="three-dots-btn popover-btn before-bg-anim"
+              >
+                <img
+                  className="three-dots-vertical"
+                  src="/three-dots-vertical.svg"
+                  alt=""
+                />
+                <div id={`popover-${index}`} className="popover">
                   <a
-                    target="_blank"
-                    href={githubUrl}
-                    className="before-bg-anim"
+                    onClick={() => {
+                      setCopied(true);
+                      setTimeout(() => {
+                        setCopied(false);
+                        document.getElementById(`btn-${index}`)?.blur();
+                      }, 1000);
+                      clipboardWrite(
+                        `https://honzoraptor.vercel.app/projects#${title.replaceAll(
+                          " ",
+                          "-"
+                        )}`
+                      );
+                    }}
+                    className="popover-link"
                   >
-                    <img src="/github.svg" className="no-select" />
+                    <span className={copied ? "success-text" : ""}>
+                      {copied ? "Link copied!" : "Copy link"}
+                    </span>
                   </a>
-                  <p className="no-demo">App is not deployed yet</p>
-                </>
-              )}
-            </>
-          )}
-        </div>
-        <div className="project-bottom-wrp">
-          <div className="tags-wrp">
-            <p>Tags:</p>
-            <div className="tags">
-              {tags.map((v: any, i: any) => {
-                return (
-                  <p key={i} className={`tag ${v}-tag`}>
-                    {v}
-                  </p>
-                );
-              })}
+                </div>
+              </button>
             </div>
           </div>
-          <div className="popover-wrp">
-            <button
-              id={`btn-${index}`}
-              className="three-dots-btn popover-btn before-bg-anim"
-            >
-              <img
-                className="three-dots-vertical"
-                src="/three-dots-vertical.svg"
-                alt=""
-              />
-              <div id={`popover-${index}`} className="popover">
-                <a
-                  onClick={() => {
-                    setCopied(true);
-                    setTimeout(() => {
-                      setCopied(false);
-                      document.getElementById(`btn-${index}`)?.blur();
-                    }, 1000);
-                    clipboardWrite(
-                      `https://honzoraptor.vercel.app/projects#${title.replaceAll(
-                        " ",
-                        "-"
-                      )}`
-                    );
-                  }}
-                  className="popover-link"
-                >
-                  <span className={copied ? "success-text" : ""}>
-                    {copied ? "Link copied!" : "Copy link"}
-                  </span>
-                </a>
-              </div>
-            </button>
+        </div>
+        <div className="align-center">
+          <div className="project-links flex-between align-center">
+            <div className="project-button-links">
+              <Link
+                target="_blank"
+                href={link}
+                className="primary-btn align-center btn-gap project-link"
+              >
+                View demo
+                <img src="/link-away.svg" className="image-height-20" />
+              </Link>
+              <Link
+                href={githubUrl}
+                className="outline-btn align-center btn-gap project-link"
+              >
+                View github
+                <img className="image-height-20" src="/github.svg" />
+              </Link>
+            </div>
+            <div className="popover-wrp grid-wrp desktop">
+              <button
+                id={`btn-${index}`}
+                className="three-dots-btn popover-btn before-bg-anim"
+              >
+                <img
+                  className="three-dots-vertical"
+                  src="/three-dots-vertical.svg"
+                  alt=""
+                />
+                <div id={`popover-${index}`} className="popover">
+                  <a
+                    onClick={() => {
+                      setCopied(true);
+                      setTimeout(() => {
+                        setCopied(false);
+                        document.getElementById(`btn-${index}`)?.blur();
+                      }, 1000);
+                      clipboardWrite(
+                        `https://honzoraptor.vercel.app/projects#${title.replaceAll(
+                          " ",
+                          "-"
+                        )}`
+                      );
+                    }}
+                    className="popover-link"
+                  >
+                    <span className={copied ? "success-text" : ""}>
+                      {copied ? "Link copied!" : "Copy link"}
+                    </span>
+                  </a>
+                </div>
+              </button>
+            </div>
           </div>
         </div>
       </div>
