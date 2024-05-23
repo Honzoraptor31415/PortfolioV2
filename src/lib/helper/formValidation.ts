205a18b9b04d954fcf7c4fe7908a2aabc6230a70
Honzoraptor <j.ackermann@email.cz>
2024-05-23 19:01:19 +0200

    Added form validation to formValidation.ts

--- src/lib/helper/formValidation.ts
+++ src/lib/helper/formValidation.ts
@@ -1,7 +1,38 @@
 export function emailCheck(email: string) {
-  return ""
+  const check = {
+    message: "",
+    isValid: true,
+  };
+
+  if (email.length < 1) {
+    check.message = "Email can't be empty";
+    check.isValid = false;
+  } else if (
+    !email.match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    ) ||
+    email.length > 255
+  ) {
+    check.message = "Invalid email";
+    check.isValid = false;
+  }
+
+  return check;
 }
 
 export function passwordCheck(password: string) {
-  return ""
+  const check = {
+    message: "",
+    isValid: true,
+  };
+
+  if (password.length < 1) {
+    check.message = "Password can't be empty";
+    check.isValid = false;
+  } else if (password.length < 6) {
+    check.message = "Password is too short";
+    check.isValid = false;
+  }
+
+  return check;
 }
