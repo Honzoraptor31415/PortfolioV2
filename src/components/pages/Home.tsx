4357fdb200346b716c2af65cec0ff593ae25541e
Honzoraptor <j.ackermann@email.cz>
2024-07-10 08:04:28 +0200

    Added a basic header

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,11 +1,17 @@
 "use client"
 
-import React from 'react'
+import BlurryBackground from "../ui/BlurryBackground"
 
 function HomePage() {
   return (
-    <header>
-
+    <header className="main-header">
+      <div className="header-side flex-column gap-20 justify-center">
+        <h1 className="gradient-text main-heading">Hey there!</h1>
+        <p>Welcome to my portfolio website! I'm a dude from Czechia who likes to code stuff, mainly with Javascript. Here you can find some of my cool projects, so if you're interested, definitely <a href="/projects">go check them out</a>.</p>
+      </div>
+      <div className="header-side flex-end align-center">
+        <BlurryBackground />
+      </div>
     </header>
   )
 }
---
+++ src/components/ui/BlurryBackground.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+function BlurryBackground() {
+  return (
+    <div className="blurry-bg-wrp">
+      <div className="blurry-bg-element"></div>
+      <div className="blurry-bg-element"></div>
+    </div>
+  )
+}
+
+export default BlurryBackground
