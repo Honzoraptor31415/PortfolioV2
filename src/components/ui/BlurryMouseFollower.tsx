4d2dcc4ef67cd30964b221923efbacea6f5871f6
Honzoraptor <j.ackermann@email.cz>
2024-07-18 16:02:41 +0200

    Minor polishment

--- src/components/ui/BlurryMouseFollower.tsx
+++ src/components/ui/BlurryMouseFollower.tsx
@@ -5,14 +5,14 @@ import { useEffect, useState } from "react"
 function BlurryMouseFollower() {
   const size = 125
 
-  const [mousePos, setMousePos] = useState({ left: 0, top: 0 })
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 })
   useEffect(() => {
     document.addEventListener("mousemove", (e) => {
-      setMousePos({ left: e.clientX, top: e.clientY })
+      setMousePos({ x: e.clientX, y: e.clientY })
     })
   }, [])
   return (
-    <div className="blurry-bg-element blurry-mouse-follower" style={{ left: mousePos.left - (size / 2), top: mousePos.top - (size / 2), "--width": `${size}px` } as React.CSSProperties}></div>
+    <div className="blurry-bg-element blurry-mouse-follower" style={{ left: mousePos.x - (size / 2), top: mousePos.y - (size / 2), "--width": `${size}px` } as React.CSSProperties}></div>
   )
 }
 
